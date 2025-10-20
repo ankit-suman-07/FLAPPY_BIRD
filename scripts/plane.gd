0d5307530109d18e6fe23f7aa226b2ce91110a46
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2025-10-21 01:28:01 +0530

    Include jump animation

---
+++ scripts/plane.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+const JUMP_POWER: float = -350.0
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var _gravity: float = ProjectSettings.get("physics/2d/default_gravity")
+
+func _ready() -> void:
+	print(_gravity)
+
+func _process(delta: float) -> void:
+	pass	
+
+func _physics_process(delta: float) -> void:
+	velocity.y += _gravity * delta
+	
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_POWER
+		animation_player.play("jump")
+	
+	move_and_slide()
