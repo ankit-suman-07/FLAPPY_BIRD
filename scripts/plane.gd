ade4b00f312372524c33197a2dfa743af9fe4ce1
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2025-10-21 19:20:53 +0530

    Create separate methods for fly and die

--- scripts/plane.gd
+++ scripts/plane.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const JUMP_POWER: float = -350.0
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 var _gravity: float = ProjectSettings.get("physics/2d/default_gravity")
 
@@ -12,10 +13,20 @@ func _process(delta: float) -> void:
 	pass	
 
 func _physics_process(delta: float) -> void:
+	fly(delta)
+	
+	move_and_slide()
+	
+	if is_on_floor():
+		die()
+		
+func fly(delta: float) -> void:
 	velocity.y += _gravity * delta
 	
 	if Input.is_action_just_pressed("jump"):
 		velocity.y = JUMP_POWER
 		animation_player.play("jump")
-	
-	move_and_slide()
+
+func die() -> void:
+	animated_sprite_2d.stop()
+	set_physics_process(false)
