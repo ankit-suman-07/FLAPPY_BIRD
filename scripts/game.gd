a454986cf57be7c2e2849f28b8f02ba81250eba3
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2025-10-22 01:15:43 +0530

    Create pipes spawner

---
+++ scripts/game.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+const PIPES = preload("uid://cp2brgy51xf6q")
+
+
+@onready var pipes_holder: Node = $pipesHolder
+@onready var upper_marker: Marker2D = $upperMarker
+@onready var lower_marker: Marker2D = $lowerMarker
+
+func _ready() -> void:
+	spawn_pipes()
+
+func spawn_pipes() -> void:
+	var new_pipes = PIPES.instantiate()
+	var y_position: float = randf_range(
+		upper_marker.position.y,
+		lower_marker.position.y
+	)
+	
+	new_pipes.position = Vector2(
+		upper_marker.position.x,
+		y_position
+	)
+	
+	pipes_holder.add_child(new_pipes)
+
+func _on_spawn_timer_timeout() -> void:
+	spawn_pipes()
