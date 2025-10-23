638433e0e8ba22e45d3b8b185bc7d657ae284d57
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2025-10-24 01:16:09 +0530

    update game over label

--- scripts/game_ui.gd
+++ scripts/game_ui.gd
@@ -1,11 +1,27 @@
 extends Control
 
-#const MAIN = preload("uid://cm621q6roibev")
+@onready var game_over: Label = $MarginContainer/gameOver
+@onready var play_again: Label = $MarginContainer/playAgain
+@onready var game_over_timer: Timer = $gameOverTimer
+
+var _can_press: bool = false
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("exit"):
 		GameManager.load_main_scene()
+	elif _can_press and event.is_action_pressed("jump"):
+		GameManager.load_main_scene()
 
-func _ready() -> void:
-	get_tree().paused = false
+func _enter_tree() -> void:
+	SignalsHub._on_plane_died.connect(on_plane_died)
 	
+
+func on_plane_died() -> void:
+	game_over.show()
+	game_over_timer.start()
+
+
+func _on_game_over_timer_timeout() -> void:
+	_can_press = true
+	game_over.hide()
+	play_again.show()
